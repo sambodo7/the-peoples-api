1c03410493afd9ccf6b13a0ad322ec286cf751ea
bodo <sstephenson@kana.com>
2018-02-09 17:37:41 +0000

    routes

--- src/index.js
+++ src/index.js
@@ -1,9 +1,36 @@
 /*eslint-env node, es6*/
-const restify = require('restify');
-const config = require( "./config" );
+const restify = require("restify");
+const config = require("./config");
 
 const server = restify.createServer();
 
-server.listen(config.port, function() {
-  console.log('%s listening at %s', server.name, server.url);
+server.get("/", (req, res, next) => {
+
+    res.send("home")
+    return next();
+
+});
+
+server.post("/tree", (req, res, next) => {
+
+    return next();
+
+});
+
+server.get("/tree", (req, res, next) => {
+
+    return next();
+
+});
+
+server.get("/tree/:treeName", (req, res, next) => {
+
+    return next();
+
+});
+
+server.listen(config.port, () => {
+
+    console.log("%s listening at %s", server.name, server.url);
+
 });
